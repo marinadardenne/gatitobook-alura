1fed83144ecd88fdae692b2bcf620c1844481edb
Marina Dardenne <marina.mdardenne@gmail.com>
2021-10-11 16:31:17 -0300

    Implementação de validação de senha/usurio iguais

--- app/home/novo-usuario/novo-usuario.component.ts
+++ app/home/novo-usuario/novo-usuario.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -23,10 +24,13 @@ export class NovoUsuarioComponent implements OnInit {
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group(
       {
-      email: ['',[Validators.required, Validators.email]],
-      fullName: ['', [Validators.required, Validators.minLength]],
-      userName: ['', [minusculoValidator], [this.usuarioExistenteService.usuarioJaExiste()]],
-      password: [''],
+        email: ['',[Validators.required, Validators.email]],
+        fullName: ['', [Validators.required, Validators.minLength]],
+        userName: ['', [minusculoValidator], [this.usuarioExistenteService.usuarioJaExiste()]],
+        password: [''],
+      },
+      {
+        validators: [usuarioSenhaIguaisValidator]
       }
     );
   }
