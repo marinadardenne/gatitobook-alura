593c7dcce4d1098885ec613bca961072dff6930b
Marina Dardenne <marina.mdardenne@gmail.com>
2021-10-17 12:52:02 -0300

    Cabeçalho e rodapé implementados

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,15 @@
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RodapeModule } from './componentes/rodape/rodape.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CabecalhoModule, RodapeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
